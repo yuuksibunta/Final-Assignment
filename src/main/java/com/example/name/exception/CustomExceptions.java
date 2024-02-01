9ac33f9e74db79f9084c7b54fdabf8a58313bbc9
yuuksibunta <idayuuki.tiu@gmail.com>
2024-02-01 21:19:20 +0900

    HTTPステータスコードを指定する例外クラスãの実装

---
+++ src/main/java/com/example/name/exception/CustomExceptions.java
@@ -0,0 +1,21 @@
+package com.example.name.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+public class CustomExceptions {
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public static class NotFoundException extends RuntimeException {
+        public NotFoundException(String message) {
+            super(message);
+        }
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public static class BadRequestException extends RuntimeException {
+        public BadRequestException(String message) {
+            super(message);
+        }
+    }
+}
