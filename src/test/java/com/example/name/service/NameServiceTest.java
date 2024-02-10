da9cfadbe2e85767afd785ee81baba5d92f1c49d
yuuksibunta <idayuuki.tiu@gmail.com>
2024-02-10 16:38:19 +0900

    ワイルドカードインポートの削除

--- src/test/java/com/example/name/service/NameServiceTest.java
+++ src/test/java/com/example/name/service/NameServiceTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
@@ -18,7 +19,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.verify;
+
 
 @ExtendWith(MockitoExtension.class)
 public class NameServiceTest {
@@ -57,7 +60,7 @@ public class NameServiceTest {
     @Test
     public void 正常に新規のユーザーが登録できること() {
         NameRequest newNameRequest = new NameRequest("kazushi", 33);
-        doNothing().when(nameMapper).insert(any(Name.class));
+        Mockito.doNothing().when(nameMapper).insert(any(Name.class));
         nameService.insert(newNameRequest);
         verify(nameMapper).insert(any(Name.class));
     }
